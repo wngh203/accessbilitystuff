863e8379a9e4d8d6d4d91f3c39b83e97e15046c8
YoonJuHo <JuHo@MacBook-Pro-2.local>
2017-11-28 00:13:13 -0500

    fuck you google

--- scripts/voiceRecog.js
+++ scripts/voiceRecog.js
@@ -1,70 +1,4 @@
-var log   = console.log.bind(this); 
-var langs =
-[['Afrikaans',       ['af-ZA']],
- ['Bahasa Indonesia',['id-ID']],
- ['Bahasa Melayu',   ['ms-MY']],
- ['Català',          ['ca-ES']],
- ['Čeština',         ['cs-CZ']],
- ['Deutsch',         ['de-DE']],
- ['English',         ['en-AU', 'Australia'],
-                     ['en-CA', 'Canada'],
-                     ['en-IN', 'India'],
-                     ['en-NZ', 'New Zealand'],
-                     ['en-ZA', 'South Africa'],
-                     ['en-GB', 'United Kingdom'],
-                     ['en-US', 'United States']],
- ['Español',         ['es-AR', 'Argentina'],
-                     ['es-BO', 'Bolivia'],
-                     ['es-CL', 'Chile'],
-                     ['es-CO', 'Colombia'],
-                     ['es-CR', 'Costa Rica'],
-                     ['es-EC', 'Ecuador'],
-                     ['es-SV', 'El Salvador'],
-                     ['es-ES', 'España'],
-                     ['es-US', 'Estados Unidos'],
-                     ['es-GT', 'Guatemala'],
-                     ['es-HN', 'Honduras'],
-                     ['es-MX', 'México'],
-                     ['es-NI', 'Nicaragua'],
-                     ['es-PA', 'Panamá'],
-                     ['es-PY', 'Paraguay'],
-                     ['es-PE', 'Perú'],
-                     ['es-PR', 'Puerto Rico'],
-                     ['es-DO', 'República Dominicana'],
-                     ['es-UY', 'Uruguay'],
-                     ['es-VE', 'Venezuela']],
- ['Euskara',         ['eu-ES']],
- ['Français',        ['fr-FR']],
- ['Galego',          ['gl-ES']],
- ['Hrvatski',        ['hr_HR']],
- ['IsiZulu',         ['zu-ZA']],
- ['Íslenska',        ['is-IS']],
- ['Italiano',        ['it-IT', 'Italia'],
-                     ['it-CH', 'Svizzera']],
- ['Magyar',          ['hu-HU']],
- ['Nederlands',      ['nl-NL']],
- ['Norsk bokmål',    ['nb-NO']],
- ['Polski',          ['pl-PL']],
- ['Português',       ['pt-BR', 'Brasil'],
-                     ['pt-PT', 'Portugal']],
- ['Română',          ['ro-RO']],
- ['Slovenčina',      ['sk-SK']],
- ['Suomi',           ['fi-FI']],
- ['Svenska',         ['sv-SE']],
- ['Türkçe',          ['tr-TR']],
- ['български',       ['bg-BG']],
- ['Pусский',         ['ru-RU']],
- ['Српски',          ['sr-RS']],
- ['한국어',            ['ko-KR']],
- ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
-                     ['cmn-Hans-HK', '普通话 (香港)'],
-                     ['cmn-Hant-TW', '中文 (台灣)'],
-                     ['yue-Hant-HK', '粵語 (香港)']],
- ['日本語',           ['ja-JP']],
- ['Lingua latīna',   ['la']]];
-
-
-
+var log         = console.log.bind(this); 
 var recognizing = false;
 var ignore_onend;
 
@@ -76,33 +10,33 @@ if (!('webkitSpeechRecognition' in window)) {
   recognition.interimResults = false;
   recognition.onstart = function() {
     recognizing = true;
-    showInfo('Info: Start talking now...');
+    showStatus('Info: Start talking now...');
   };
   recognition.onerror = function(event) {
     if (event.error == 'no-speech') {
-      showInfo('Error: No speech given.');
+      showStatus('Error: No speech given.');
       ignore_onend = true;
     }
     if (event.error == 'audio-capture') {
-      showInfo('Error: No mic found.');
+      showStatus('Error: No mic found.');
       ignore_onend = true;
     }
     if (event.error == 'not-allowed') {
-        showInfo("Error: Voice recog not allowed.")
+        showStatus("Error: Voice recog not allowed.")
       ignore_onend = true;
     }
   };
   recognition.onend = function() {
     recognizing = false;
       
-    showInfo("Info: Voice Recog Ended.");
+    showStatus("Info: Voice Recog Ended.");
       
     if (ignore_onend) {
       return;
     }
       
 //    if (!final_transcript) {
-//      showInfo('info_start');
+//      showStatus('info_start');
 //      return;
 //    }
       
@@ -113,10 +47,42 @@ if (!('webkitSpeechRecognition' in window)) {
         result += event.results[i][0].transcript;
     }
     showResult(result);
+    action(result);
   };
 }
 
-function showInfo (status) {
+function action (result) {
+    var result_arr = result.split(" ");
+    if (result_arr[0].toLowerCase() == "scroll") {
+        if (result_arr.length != 2) return;
+        if (result_arr[1].toLowerCase() == "up") {
+            window.scrollBy(0,-100);
+            return;
+        }
+        else if (result_arr[1].toLowerCase() == "down") {
+            window.scrollBy(0,100);
+            return;
+        }
+        else {
+            return;
+        }
+    }
+    
+    else if (result_arr[0].toLowerCase() == "click") {
+        
+    }
+    
+    else if (result_arr[0].toLowerCase() == "enter") {
+        
+    }
+    
+    else {
+        alert("Fuck you.");
+        return;
+    }
+}
+
+function showStatus (status) {
     log(status);
     $("#status").html(status);
 }
@@ -125,13 +91,11 @@ function showResult (result) {
     $("#result").html(result);
 }
 
-$(document).ready(function() {
-    
+$("#fuckme").click(function () {
     if (recognizing == false) {
         recognition.start();
-        showInfo("Voice Recog started.");
+        showStatus("Info: Voice Recog started.");
     } 
-    
 });
     
 
